915a4f1bdb8e46d87ed760f8cbf784265d768a2a
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-02 21:08:51 +0500

    Change font family for nav bar + Add links for routing

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import "../css/navBar.css";
 import { FaFantasyFlightGames } from "react-icons/fa";
 import { RiShoppingCartLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -12,12 +13,20 @@ const NavBar = () => {
         </div>
         <div className="nav-all-list">
           <li className="nav-list">
-            <ul>Products</ul>
-            <ul>On Sale</ul>
-            <ul>Contact Us</ul>
-            <ul>Log In</ul>
+            <Link to={"/products"}>
+              <ul>Products</ul>
+            </Link>
+            <Link to={"/on-sales"}>
+              <ul>On Sale</ul>
+            </Link>
+            <Link to={"/contact-us"}>
+              <ul>Contact Us</ul>
+            </Link>
+            <Link to={"/login"}>
+              <ul>Log In</ul>
+            </Link>
             <ul>
-              <RiShoppingCartLine className="nav-cart-icon"/>
+              <RiShoppingCartLine className="nav-cart-icon" />
             </ul>
           </li>
         </div>
