fa93338aa29c35ea0fd74528fe3762167d67863a
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-02 20:52:03 +0500

    Add icons to nav bar + Style nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,24 @@
 import "../css/navBar.css";
+import { FaFantasyFlightGames } from "react-icons/fa";
+import { RiShoppingCartLine } from "react-icons/ri";
 
 const NavBar = () => {
   return (
     <>
       <div className="nav-bar">
         <div className="nav-logo">
+          <FaFantasyFlightGames className="nav-logo-icon" />
           <span className="nav-logo-name">GAMEFORGE</span>
         </div>
-        <div className="nav-list">
-          <li className="list">
+        <div className="nav-all-list">
+          <li className="nav-list">
             <ul>Products</ul>
             <ul>On Sale</ul>
             <ul>Contact Us</ul>
             <ul>Log In</ul>
-            <ul>Cart</ul>
+            <ul>
+              <RiShoppingCartLine className="nav-cart-icon"/>
+            </ul>
           </li>
         </div>
       </div>
