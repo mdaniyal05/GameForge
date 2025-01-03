dfcb9f8b4e7a2cb69c5ae8581b01cc32679b34a8
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-03 09:38:39 +0500

    Create hero poster card component + Create css for hero poster card + Add image for poster + Add global default style in index css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import HeroPosterCard from "./components/HeroPosterCard";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <HeroPosterCard />
     </>
   );
 }
---
+++ frontend/src/components/HeroPosterCard.jsx
@@ -0,0 +1,11 @@
+import "../css/heroPosterCard.css";
+
+const HeroPosterCard = () => {
+  return (
+    <>
+      <div className="hero-card">Card</div>
+    </>
+  );
+};
+
+export default HeroPosterCard;
