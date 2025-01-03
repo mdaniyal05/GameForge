50468335b74aef8c19cd13a54ecc320b1763d1d4
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-03 14:28:47 +0500

    Create hero buy card component + Create hero buy card  css + Add styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import HeroPosterCard from "./components/HeroPosterCard";
+import HeroBuyCard from "./components/HeroBuyCard";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
-      <HeroPosterCard />
+      <HeroBuyCard/>
     </>
   );
 }
