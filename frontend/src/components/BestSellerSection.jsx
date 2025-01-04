e7c43b2b89681fd176ec5921efae3f5da5b37226
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-04 14:21:30 +0500

    Add styling + Add more stuff to best seller section

--- frontend/src/components/BestSellerSection.jsx
+++ frontend/src/components/BestSellerSection.jsx
@@ -3,8 +3,16 @@ import "../css/bestSellerSection.css";
 const BestSellerSection = () => {
   return (
     <>
-      <div className="best-seller">
-        <h1>Best Seller Section</h1>
+      <div className="best-seller-container">
+        <div className="best-seller-heading">
+          <h2>BEST SELLERS</h2>
+          <div className="view-all">
+            <span>View All</span>
+          </div>
+        </div>
+        <div className="product-slider-container">
+          <h1>Slider</h1>
+        </div>
       </div>
     </>
   );
