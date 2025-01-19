3550710d791a6c0fc3d12c80cd74e6d2b44122bb
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-19 14:46:07 +0500

    Modify layout of category section + Add basic layout styling

--- frontend/src/components/CategorySection.jsx
+++ frontend/src/components/CategorySection.jsx
@@ -4,19 +4,23 @@ const CategorySection = () => {
   return (
     <>
       <div className="category-section-cont">
-        <span className="category-heading">SHOP BY CATEGORY</span>
-        <div className="categories-cont">
-          <div className="consoles-cont">
-            <div className="consoles"></div>
-            <span className="consoles-heading">Consoles</span>
-          </div>
-          <div className="accessories-cont">
-            <div className="accessories"></div>
-            <span className="accessories-heading">Accessories</span>
-          </div>
-          <div className="controllers-cont">
-            <div className="controllers"></div>
-            <span className="controllers-heading">Controllers</span>
+        <div className="category-section-cont2">
+          <h2 className="category-heading">
+            <span>SHOP BY CATEGORY</span>
+          </h2>
+          <div className="categories-cont">
+            <div className="consoles-cont">
+              <div className="consoles"></div>
+              <span className="consoles-heading">Consoles</span>
+            </div>
+            <div className="accessories-cont">
+              <div className="accessories"></div>
+              <span className="accessories-heading">Accessories</span>
+            </div>
+            <div className="controllers-cont">
+              <div className="controllers"></div>
+              <span className="controllers-heading">Controllers</span>
+            </div>
           </div>
         </div>
       </div>
