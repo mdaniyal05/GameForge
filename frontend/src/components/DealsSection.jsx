5ae9824e8b962a572f8a925becbf3871bdc62b74
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-20 10:24:33 +0500

    Add cards to deals section + Add images for the cards

--- frontend/src/components/DealsSection.jsx
+++ frontend/src/components/DealsSection.jsx
@@ -1,9 +1,22 @@
 import "../css/dealsSection.css";
+import DealsSectionCard from "../components/DealsSectionCard";
 
 const DealsSection = () => {
   return (
     <>
-      <div className="deals-section-cont"></div>
+      <div className="deals-section-cont">
+        <div className="cards-cont">
+          <DealsSectionCard
+            cardImg={"./chronoSplitCover.jpg"}
+            cardName={"Chrono Split"}
+          />
+          <DealsSectionCard
+            cardImg={"./deadAtLastCover.jpg"}
+            cardName={"Dead At Last"}
+          />
+          <DealsSectionCard cardImg={"./kiraCover.jpg"} cardName={"Kira"} />
+        </div>
+      </div>
     </>
   );
 };
