eafabf806e682484ef0dab2dfbca81cbe88341ae
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-20 12:51:35 +0500

    Create discount section component + Add image

---
+++ frontend/src/components/DiscountSection.jsx
@@ -0,0 +1,13 @@
+const DiscountSection = () => {
+  return (
+    <>
+        <div className="discount-cont">
+            <span>SPEND & SAVE</span>
+            <span>Save 20% when you spend more than $125</span>
+            <button></button>
+        </div>
+    </>
+  );
+};
+
+export default DiscountSection;
