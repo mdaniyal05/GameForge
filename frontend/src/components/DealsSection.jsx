e3d85767dac8b5a98aca8466f5e3fdb95dbfab5e
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-20 11:23:24 +0500

    Edit layout + Add styling

--- frontend/src/components/DealsSection.jsx
+++ frontend/src/components/DealsSection.jsx
@@ -4,26 +4,24 @@ const DealsSection = () => {
   return (
     <>
       <div className="deals-section-cont">
-        <div className="cards-cont">
-          <div className="chrono-split">
-            <img
-              src="./chronosplitCover.jpg"
-              alt="Chrono Split"
-              width={400}
-              height={530}
-            />
-          </div>
-          <div className="dead-at-last">
-            <img
-              src="./deadAtLastCover.jpg"
-              alt="Dead At Last"
-              width={400}
-              height={530}
-            />
-          </div>
-          <div className="kira">
-            <img src="./kiraCover.jpg" alt="Kira" width={400} height={530} />
-          </div>
+        <div className="chrono-split">
+          <img
+            src="./chronosplitCover.jpg"
+            alt="Chrono Split"
+            width={400}
+            height={530}
+          />
+        </div>
+        <div className="dead-at-last">
+          <img
+            src="./deadAtLastCover.jpg"
+            alt="Dead At Last"
+            width={400}
+            height={530}
+          />
+        </div>
+        <div className="kira">
+          <img src="./kiraCover.jpg" alt="Kira" width={400} height={530} />
         </div>
       </div>
     </>
