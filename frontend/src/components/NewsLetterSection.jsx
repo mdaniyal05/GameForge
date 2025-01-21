dcc6969deff924a8d05f913c3568b780cfb8dbba
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-21 13:04:39 +0500

    Modify layout

--- frontend/src/components/NewsLetterSection.jsx
+++ frontend/src/components/NewsLetterSection.jsx
@@ -3,7 +3,28 @@ import "../css/newsLetterSection.css";
 const NewsLetterSection = () => {
   return (
     <>
-      <div className="news-letter-cont"></div>
+      <div className="news-letter-cont">
+        <div className="heading-cont">
+          <h2>
+            <span className="news-letter-heading">NEWSLETTER</span>
+          </h2>
+          <p className="news-letter-desc">
+            Sign up to receive updates on new products and special offers
+          </p>
+        </div>
+        <div className="email-cont">
+          <div className="email-input">
+            <label htmlFor="email">Email</label>
+            <input type="email" />
+          </div>
+          <div className="email-input2">
+            <input type="checkbox" />
+            <label htmlFor="checkbox">
+              Yes, subscribe me to your newsletter.
+            </label>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
