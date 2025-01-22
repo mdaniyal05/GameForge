dbc3272dec9577d147c8d3a6385001723cd73293
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-01-22 09:11:24 +0500

    Create games stack component + Add images

---
+++ frontend/src/components/GamesStack.jsx
@@ -0,0 +1,11 @@
+const GamesStack = () => {
+  return (
+    <>
+      <div className="games-stack-cont">
+        <div className="games-stack"></div>
+      </div>
+    </>
+  );
+};
+
+export default GamesStack;
