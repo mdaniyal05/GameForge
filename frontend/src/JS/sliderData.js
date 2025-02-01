d3880b35c62399bb813c35d375bebbe992777bbe
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-01 18:15:59 +0500

    Create slider data

---
+++ frontend/src/JS/sliderData.js
@@ -0,0 +1,32 @@
+export const bestSellerData = [
+  {
+    img: "/gamingChair.jpg",
+    name: "Mach Gaming Chair",
+    price: `${116.99}`,
+  },
+  {
+    img: "/headset.jpg",
+    name: "Echo Headset",
+    price: `${39.99}`,
+  },
+  {
+    img: "/mechanicalKeyboard.jpg",
+    name: "Spartan Mechanical Keyboard",
+    price: `${40.49}`,
+  },
+  {
+    img: "/playBox.jpg",
+    name: "PlayBox XZ Gold Edition",
+    price: `${1000.0}`,
+  },
+  {
+    img: "/waveGen.jpg",
+    name: "Wave Gen RX",
+    price: `${579.99}`,
+  },
+  {
+    img: "/wirelessMouse.jpg",
+    name: "X-2 Wireless Mouse",
+    price: `${24.99}`,
+  },
+];
