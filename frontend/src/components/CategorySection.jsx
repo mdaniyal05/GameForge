c4e460009e78521f8e612aaa10a3267d919c96b5
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-14 21:22:41 +0500

    Add animation + Fix

--- frontend/src/components/CategorySection.jsx
+++ frontend/src/components/CategorySection.jsx
@@ -1,13 +1,33 @@
 import "../css/categorySection.css";
 import CategorySectionCard from "./CategorySectionCard";
+import { motion } from "framer-motion";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
 const CategorySection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <>
       <div className="category-section-cont">
         <div className="category-section-cont2">
-          <h2 className="category-heading">
-            <span>SHOP BY CATEGORY</span>
+          <h2 className="category-heading" ref={ref}>
+            {isInView ? (
+              <motion.span
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 1,
+                  delay: 0.5,
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+              >
+                SHOP BY CATEGORY
+              </motion.span>
+            ) : (
+              <></>
+            )}
           </h2>
           <div className="categories-cont">
             <CategorySectionCard
--- frontend/src/components/SellersSection.jsx
+++ frontend/src/components/SellersSection.jsx
@@ -21,7 +21,6 @@ const SellersSection = ({ mainHeading, sellerData }) => {
         <div className="seller-heading" ref={ref}>
           {isInView ? (
             <motion.h2
-              ref={ref}
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
