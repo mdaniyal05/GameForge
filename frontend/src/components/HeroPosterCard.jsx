42863f84c7c3bc862c3bf6bfb1d20a79ec1cc2fe
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-14 14:32:55 +0500

    Add animation

--- frontend/src/components/HeroPosterCard.jsx
+++ frontend/src/components/HeroPosterCard.jsx
@@ -1,7 +1,19 @@
 import "../css/heroPosterCard.css";
+import { motion } from "framer-motion";
 
 const HeroPosterCard = () => {
-  return <div className="hero-card"></div>;
+  return (
+    <motion.div
+      initial={{ y: 500 }}
+      animate={{ y: 0 }}
+      transition={{
+        type: "spring",
+        duration: 1.5,
+        bounce: 0.4,
+      }}
+      className="hero-card"
+    ></motion.div>
+  );
 };
 
 export default HeroPosterCard;
