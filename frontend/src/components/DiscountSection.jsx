5c248742be2c45c942857fe4da274b3ee2998c36
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-15 14:43:53 +0500

    Add animation

--- frontend/src/components/DiscountSection.jsx
+++ frontend/src/components/DiscountSection.jsx
@@ -1,19 +1,34 @@
 import "../css/discountSection.css";
 import Button from "./Button";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const DiscountSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <>
-      <div className="discount-cont">
+      <div className="discount-cont" ref={ref}>
         <span className="spend-save">SPEND & SAVE</span>
         <span className="save">Save 20% when you spend more than $125</span>
-        <Button
-          text={"Shop Now"}
-          bgColor={"#8355e5"}
-          color={"white"}
-          height={"50px"}
-          width={"155px"}
-        />
+        {isInView ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
+            <Button
+              text={"Shop Now"}
+              bgColor={"#8355e5"}
+              color={"white"}
+              height={"50px"}
+              width={"155px"}
+            />
+          </motion.div>
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );
