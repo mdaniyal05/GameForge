07a60ddb460cd6b54ae54275fab2dfac6f9bd057
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-15 14:49:34 +0500

    Add animation

--- frontend/src/components/NewsLetterSection.jsx
+++ frontend/src/components/NewsLetterSection.jsx
@@ -1,16 +1,30 @@
 import "../css/newsLetterSection.css";
 import Button from "./Button";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const NewsLetterSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
     <>
-      <div className="news-letter-cont">
-        <div className="heading-cont">
-          <h2 className="news-letter-heading">NEWSLETTER</h2>
-          <p className="news-letter-desc">
-            Sign up to receive updates on new products and special offers
-          </p>
-        </div>
+      <div className="news-letter-cont" ref={ref}>
+        {isInView ? (
+          <motion.div
+            className="heading-cont"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
+            <h2 className="news-letter-heading">NEWSLETTER</h2>
+            <p className="news-letter-desc">
+              Sign up to receive updates on new products and special offers
+            </p>
+          </motion.div>
+        ) : (
+          <></>
+        )}
         <div className="email-cont">
           <div className="email-input">
             <label htmlFor="email">Email *</label>
